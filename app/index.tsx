71a98a0e957308ecc1b74a009667e121c77f4717
StevenLeCamPro <steven.lecam@outlook.fr>
2025-02-24 17:20:08 +0100

    ajout login + supp lien header

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, Image, Text, View } from "react-native";
 import Header from "./header";
+import Login from "./login";
 
 export default function Index() {
   return (
     <SafeAreaView>
       <Header/>
       <View style={styles.content}>
+
+      <Login/>
 
       </View>
     </SafeAreaView>
@@ -16,6 +19,8 @@ export default function Index() {
 const styles = StyleSheet.create(
   {
     content: {
+      backgroundColor: '#f0f0f0',  // à changer
+   
 
     }
   }
---
+++ app/login.tsx
@@ -0,0 +1,101 @@
+import { Link } from "expo-router";
+import React from "react";
+import { StyleSheet, SafeAreaView, Image, Text, View, Pressable } from "react-native";
+import { TextInput, Button } from "react-native";
+
+export default function Login() {
+  return (
+    <SafeAreaView>
+      <View style={styles.content}>
+<Text style={styles.loginText}>
+    Bonjour et bienvenue ! Veuillez vous connecter. 
+   
+</Text>
+
+<View style={styles.inputContainer}>
+    <Text style={styles.formText}>
+        Rentrez vos informations ci-dessous pour vous connecter
+    </Text>
+    
+    <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+    />
+    <TextInput
+        style={styles.input}
+        placeholder="Mot de passe"
+        secureTextEntry
+    />
+    <Pressable style={styles.button}>
+        <Link href={"/"} style={styles.link}>Se connecter</Link>
+    </Pressable>
+
+    <Text style={styles.formText2}>
+    Si vous n'avez pas de compte, veuillez consulter notre site pour vous inscrire
+    </Text>
+</View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    content: {
+
+    },
+    loginText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#333',
+      textAlign: 'center',
+      marginBottom: 30,
+      marginTop: 50,
+      
+    },
+    formText: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#333',
+      textAlign: 'center',
+      marginBottom: 20,
+    },
+    formText2 : {
+        fontSize: 13,
+        color: '#333',
+        textAlign: 'center',
+    },
+    inputContainer: {
+      marginBottom: 20,
+      marginHorizontal: 30,
+      backgroundColor: '#fff',
+      padding: 20,
+      borderRadius: 15,
+      shadowColor: '#000',
+    },
+    input: {
+      height: 40,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      marginBottom: 20,
+      padding: 10,
+      borderRadius: 15,
+    },
+    button:{
+        height : 50,
+        backgroundColor: '#00A95C',
+        borderRadius: 15,   
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 50,
+        marginBottom : 20,
+    },
+    link:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    }
+  }
+)
